e7cc50a2e816f1ee032d281f9feb3d64d6cccba7
Homen Shum <77596827+HomenShum@users.noreply.github.com>
2023-02-02 18:47:39 -0800

    debug = false

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__': #only when web server is run directly
-    app.run(debut=False, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0')
     #app.run(debug=True) #changes to code will be reflected
