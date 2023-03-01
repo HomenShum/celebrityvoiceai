98ada46c894c6a18d7c21590685ac0ca7e09d9df
Homen Shum <77596827+HomenShum@users.noreply.github.com>
2023-03-01 14:25:29 -0800

    3.1.23 Update
    Updated persona selection feature, UI updates, bug fixes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from website import create_app
+
 app = create_app()
 
 if __name__ == '__main__': #only when web server is run directly
     app.run(debug=False, host='0.0.0.0')
-    #app.run(debug=True) #changes to code will be reflected
